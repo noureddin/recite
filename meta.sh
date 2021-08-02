f65fd243145a94b45ab519e3f889d909e4b20ac1
Noureddin <noureddin95@gmail.com>
2021-08-02 13:49:55 +0200

    add meta data, og:, and twitter: tags & thumb imgs

---
+++ meta.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+URL='https://noureddin.github.io/recite/'
+TITLE='راجع ما تحفظ من القرآن الكريم | رسايت'
+DESC='تطبيق ويب مجاني لمراجعة حفظ القرآن الكريم بدون كتابة. للحاسوب والمحمول.'
+
+# https://iamturns.com/open-graph-image-size/
+OG_IMG='./og-image.png'       # 1200x630; 1.91:1
+TW_IMG='./twitter-image.png'  # 1260x630; 2:1
+
+
+# General
+echo '<title>'"$TITLE"'</title>'
+echo '<meta name="description" content="'"$DESC"'">'
+echo '<link rel="canonical" href="'"$URL"'">'
+
+
+# Open Graph (Facebook et al)
+# OG doesn't follow ISO for Arabic: https://developers.facebook.com/docs/internationalization
+echo '<meta property="og:locale" content="ar_AR">'
+echo '<meta property="og:type" content="website">'
+echo '<meta property="og:title" content="'"$TITLE"'">'
+echo '<meta property="og:description" content="'"$DESC"'">'
+echo '<meta property="og:image" content="'"$OG_IMG"'">'
+echo '<meta property="og:url" content="'"$URL"'">'
+
+
+# Twitter
+# https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup
+# https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/summary-card-with-large-image
+echo '<meta property="twitter:card" content="summary_large_image">'
+echo '<meta property="twitter:url" content="'"$URL"'">'
+echo '<meta property="twitter:image" content="'"$TW_IMG"'">'
+# Twitter uses og:title if twitter:title doesn't exist,
+# and og:description if twitter:description doesn't exist.
