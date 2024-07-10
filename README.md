830b5ff459eede7779bbbbbe30ce93d2c3350c19
noureddin <noureddin@protonmail.com>
2024-07-10 21:49:35 +0200

    add translations of the meanings of the Quran
    also add Arabic Tafsir Jalalayn and update Arabic Tafsir Muyassar.

--- README.md
+++ README.md
@@ -188,7 +188,9 @@ These are not changeable from the UI, only from the URL parameters; they are exp
 
 Many thanks for [Khaled Hosny](https://github.com/khaledhosny/) for his work on [Quran Data](https://github.com/aliftype/quran-data) and [Amiri Font](https://www.amirifont.org/).
 
-Tafasir are [from Ayat](https://gist.github.com/noureddin/36742c87431e4312957b8e42d28ff9f4).
+Arabic Tafsir Muyassar is from [KFGQPC](https://qurancomplex.gov.sa/techquran/dev/).
+Arabic Tafsir Jalalayn, and all translations, are from [Tanzil](https://tanzil.net/trans/).
+All other Arabic Tafasir are [from Ayat](https://gist.github.com/noureddin/36742c87431e4312957b8e42d28ff9f4).
 
 Heartfelt thanks for the [Verse By Verse MP3 Quran](http://www.versebyversequran.com/) project,
 for providing the audio recitations for every project that needs them,
--- tafsir.js
+++ tafsir.js
@@ -5,7 +5,6 @@ const spinner = '<svg id="spinner-svg" viewBox="0 0 100 100" xmlns="http://www.w
 // all are the first ayah of a sura. the last three are the first ayah of a juz.
 const sep_ayah = [ 0, 7, 293, 493, 669, 789, 954, 1160, 1235, 1364, 1473, 1596, 1707, 1750, 1802, 1901, 2029, 2140, 2250, 2348, 2483, 2595, 2673, 2791, 2855, 2932, 3159, 3252, 3340, 3409, 3469, 3503, 3533, 3606, 3660, 3705, 3788, 3970, 4058, 4133, 4218, 4272, 4325, 4414, 4473, 4510, 4545, 4583, 4612, 4630, 4675, 4735, 4784, 4846, 4901, 4979, 5075, 5104, 5241, 5672, 6236 ]
 
-const whole = new Set(['ar_ma3any', 'ar_muyassar'])
 const tafsir = {}
 
 const part_num = (i) => sep_ayah.findIndex(a => i <= a)  // assumption: 0 <= i <= 6236
@@ -16,17 +15,20 @@ function tv (i) {
   const name = el_tafsir.value
   // const title = el_tafsir.Q('[value="'+el_tafsir.value+'"]').innerText
   const title = el_tafsir.innerHTML.match('value="'+el_tafsir.value+'"[^<>]*>([^<>]+)')[1]
+  const t = name.match(/_/) && name.match(/^ar_/) == null ? 'ترجمة '+title : title
+  const lang = (() => { const m = name.match(/^([a-z]+)_/); return m ? m[1] : 'ar' })()
+  const attr = lang !== 'ar' ? ` lang="${lang}" dir="ltr" ` : ' '
   // show tafsir
   el_tvc.style.display = 'block'
   show_el(el_tvc)
   // compose header
   const s = sura_of(i) - 1
   const aya = ayat.uthm[i-1].replace(/[#A-Z<>]+/g, '')
-  const head = `<p>${title} للآية ${filter_aaya_input(i - sura_offset[s])} من سورة ${suar_name[s]}</p><p id="tafsirnote">(يمكن تغيير التفسير من «الخيارات» في أعلى الصفحة بعد إغلاق التفسير)</p><p class="aya">${aya}</p><hr>`
+  const head = `<p>${t} للآية ${filter_aaya_input(i - sura_offset[s])} من سورة ${suar_name[s]}</p><p id="tafsirnote">(يمكن تغيير التفسير من «الخيارات» في أعلى الصفحة بعد إغلاق التفسير)</p><p class="aya">${aya}</p><hr>`
   // loading screen
   el_tv.innerHTML = head + spinner
   // get the tafsir
-  get_tafsir(name, i, (content) => { el_tv.innerHTML = head + '<div id="tvtxt">'+content+'</div>' })
+  get_tafsir(name, i, (content) => { el_tv.innerHTML = head + '<div'+attr+'id="tvtxt">'+content+'</div>' })
 }
 
 el_tvx.onclick = () => {
@@ -46,7 +48,7 @@ function get_tafsir (name, i, callback) {
 }
 
 function load_tafsir (name, i, callback) {
-  if (whole.has(name)) {
+  if (name.match(/_/)) {  // one file
     if (tafsir[name]) { callback(tafsir[name][i-1]); return }
     G('rt/'+name+'.gz').then((txt) => {
       tafsir[name] = txt.split('\n')
