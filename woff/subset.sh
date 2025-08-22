b7324a82f3df0954fa3854fde739a0ba112d8f11
noureddin <noureddin@protonmail.com>
2025-08-22 20:10:21 +0300

    include Arabic digits & imlaai tashkeel in Cortoba
    and include the source TTF and the subsetting script.

---
+++ woff/subset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input="ae_Cortoba.ttf"
+output="${input%.*}-subset"
+range=20,21,28,29,2e,3a,640,64b-652,660-669,621-63a,641-64a
+# all imlaai arabic letters, the five .():!, tatweel, all eight imlaai tashkeel marks, ٠ to ٩, and ascii space
+
+pyftsubset "$input" --output-file="$output".woff2 --layout-features=* --flavor=woff2 --unicodes=$range
+pyftsubset "$input" --output-file="$output".woff  --layout-features=* --flavor=woff  --unicodes=$range --with-zopfli
+
+
